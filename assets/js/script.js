38b4f9c21a653e4145130df751686f668d061633
Certificate Generation Aumation <marisankar4002@gmail.com>
2024-03-30 20:38:37 +0530

    modified:   Design Team.html 	modified:   assets/css/styles.css 	new file:   assets/images/Dhruv2.jpg 	new file:   assets/images/Harish.jpg 	modified:   assets/images/Sweatha.jpg 	new file:   assets/images/ananya.jpg 	new file:   assets/images/depak.jpg 	new file:   assets/images/harini.jpg 	new file:   assets/images/indra2.jpg 	new file:   assets/images/nishanth.jpg 	new file:   assets/images/sudharsan.jpg 	new file:   assets/images/ujjain.jpg 	modified:   assets/js/script.js 	modified:   automation-team.html 	modified:   council.html 	modified:   duplicate-credential.html 	modified:   executive.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,32 @@ const images = document.querySelectorAll('.image-container img');
       document.getElementById('displayed-image').innerHTML = image.outerHTML;
     });
   });
+
+  // Toggle dropdown menu
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  // Function to handle "Contact Issuer" click
+  function contactIssuer() {
+    alert("Contact Issuer clicked!");
+  }
+  
+  // Function to handle "More Information" click
+  function showMoreInformation() {
+    alert("More Information clicked!");
+  }
+  
